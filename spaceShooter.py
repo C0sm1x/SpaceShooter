78159c328d4b005944c6aa68f0e7910334a82b15
Anthony <C0sm1x@users.noreply.github.com>
2017-03-02 20:56:25 -0600

    Added a sound effect when a shot is fired, and a titlescreen.py file which will become the title screen (possibly anyway)

--- spaceShooter.py
+++ spaceShooter.py
@@ -1,6 +1,11 @@
 import pygame, random, os
 
 pygame.init()
+
+#Initializing sound!
+pygame.mixer.pre_init(44100, -16, 2, 2048)
+pygame.mixer.init()
+
 #Screen size varables
 screenWidth = 640
 screenHeight = 480
@@ -20,10 +25,12 @@ black = (0, 0, 0)
 
 clock = pygame.time.Clock()
 
-spaceShipDir = os.path.join("Sprites", "Spaceship.png")
-spaceShip = pygame.image.load(spaceShipDir)
-enemyShipDir = os.path.join("Sprites", "Red Spaceship.png")
-enemyShip = pygame.image.load(enemyShipDir)
+# Loading the spaceship images
+spaceShip = pygame.image.load(os.path.join("Sprites", "Spaceship.png"))
+enemyShip = pygame.image.load(os.path.join("Sprites", "Red Spaceship.png"))
+
+# Loading sound
+fireSound = pygame.mixer.Sound(os.path.join("sfx", "fire.ogg"))
 
 def player(playerX, playerY):
     #Drawing the player's ship to the screen
@@ -55,7 +62,7 @@ def gameloop():
     playerYVelocity = 0
  
     # Enemy variables
-    enemyX = rightBoundery + 80
+    enemyX = rightBoundery + 100
     enemyY = random.randint(topBoundery, bottomBoundery)
 
     # The variables for the bullet 
@@ -65,6 +72,10 @@ def gameloop():
     bulletHeight = 4
     bulletXVelocity = 0
     playerFired = False
+    
+    #variables for the sound, and delay of when the sound can be played again
+    delay = 300
+    soundCanPLay = False
     
     while running:
         # Setting screen color to black
@@ -85,7 +96,14 @@ def gameloop():
                     playerYVelocity = 5
                 if event.key == pygame.K_SPACE:
                     playerFired = True
-                                
+                    soundCanPlay = True
+                    while delay > 0:
+                        soundCanPLay = True
+                        fireSound.play()
+                        delay = delay - 1
+                        if delay < 0:
+                            soundCanPlay = False
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                     playerXVelocity = 0
@@ -107,11 +125,16 @@ def gameloop():
         if playerFired == False:
             bulletX = playerX
             bulletY = playerY + spriteHeight / 2
+            soundCanPlay = True
+            # Setting sound back to 300 so that sound can play again
+            delay = 300
+
         if playerFired == True:
             # What to do if the bullet's x cordinate isn't equal to the right boundery
             if bulletX != rightBoundery:
                 bullet(bulletX, bulletY, bulletWidth, bulletHeight)
                 bulletXVelocity = 10 
+
             # What to d if the x cordinate of the bullet is greater than the right boundery
             if bulletX >= rightBoundery:
                 # Sets the playerFired boolean back to false
@@ -122,31 +145,29 @@ def gameloop():
         # Player movement        
         playerX += playerXVelocity
         playerY += playerYVelocity
-
+        enemyX -= 4
         #Buller movement
         bulletX += bulletXVelocity
 
         # Calling the player function which was defined above to draw the player
         player(playerX, playerY)
         enemy(enemyX, enemyY)
-
+        # What happens if the enemy ship reaches the end of the screen, or gets hit by a bullet
         if enemyX < leftBoundery or bulletY > enemyY and bulletY < enemyY + spriteHeight and bulletX > enemyX and bulletX < enemyX + spriteWidth and playerFired == True:
             playerFired = False
-            enemyX = rightBoundery + 80
+            enemyX = rightBoundery + 100
             enemyY = random.randint(topBoundery, bottomBoundery)
             enemy(enemyX, enemyY)
  
-
-
-        enemyX -= 4
-         # Updating the display
+        # Updating the display
         pygame.display.update()
-         # FPS clock.tick(60) 
+        # FPS clock.tick(60) 
         clock.tick(60)
 
 # Running the gameloop function
 gameloop()
 
 #exiting pygame and Python
+pygame.mixer.quit()
 pygame.quit()
 quit()
